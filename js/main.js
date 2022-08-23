b32eee278ded4027cc55b55454131c539ecdcab7
Santin Liam <94681785+liam-santin@users.noreply.github.com>
2022-08-23 09:49:07 +0200

    correction exercice celsius et fin

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 /**
  * Fichier de base pour application web HTML/CSS/JS
- * @author  Steve Fallet
+ * @author  LIam Santin
  * @version 0.1
- * @since   2019-08-19
+ * @since   2022-08-23
  */
 
 (function main() {
