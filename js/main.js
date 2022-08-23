461a76baae7f4b0d8b529350747d5194b26ffbe1
Santin Liam <94681785+liam-santin@users.noreply.github.com>
2022-08-23 09:43:15 +0200

    fin exercice celsius

--- js/main.js
+++ js/main.js
@@ -6,10 +6,13 @@
  */
 
 (function main() {
-  'use strict'; // Demande un interprétation stricte du code
+    'use strict'; // Demande un interprétation stricte du code
 
-  let a = 'Bonjour';
+    const tempCelsius = Number(prompt('Température en Celsius : '));
 
-  console.log(a);
+    alert(`${tempCelsius} °C = ${tempCelsius * 9 / 5 + 32} °F`);
+
 }()); // Main IIFE
+
+
 
